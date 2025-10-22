890e159fb2b3a742fea53909ef28e995016b5e1a
Akshat Srivastava <srivastavaakshat52@gmail.com>
2025-10-23 01:26:33 +0530

    backgrountes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-amber-200'>Hello</div>
-  )
+    <div className="min-h-screen body-bg text-gray-200">
+      <div className="max-w-[1300px] mx-auto px-6 py-10 flex gap-8">
+        {/* Left half - empty */}
+        <div className="w-1/2"></div>
+
+        {/* Right half */}
+        <div className="w-1/2 flex flex-col gap-8">
+          {/* Widgets will go here */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
+
+
